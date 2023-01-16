365f004367c56d0ab61939d3a17c567ababc52cb
Matt Welsh <matt@fixie.ai>
2023-01-16 10:14:34 -0800

    Fix GraphQL API

--- fixie/client.py
+++ fixie/client.py
@@ -60,11 +60,7 @@ class FixieClient:
             headers={"Authorization": f"Bearer {self._api_key}"},
         )
         self._gqlclient = Client(transport=transport, fetch_schema_from_transport=False)
-
-        if not session_id:
-            self._session_id = self._create_session()
-        else:
-            self._session_id = session_id
+        self._session_id = session_id
 
     @property
     def gqlclient(self) -> Client:
@@ -73,18 +69,16 @@ class FixieClient:
 
     def agents(self) -> Dict[str, Dict[str, str]]:
         """Return metadata about all running Fixie Agents. The keys of the returned
-        dictionary are the Agent IDs, and the values are dictionaries containing
+        dictionary are the Agent handles, and the values are dictionaries containing
         metadata about each Agent."""
 
         query = gql(
             """
             query getAgents {
                 allAgents {
-                    agentId
+                    handle
                     name
                     description
-                    developer
-                    moreInfo
                 }
             }
         """
@@ -92,35 +86,40 @@ class FixieClient:
         result = self._gqlclient.execute(query)
         assert "allAgents" in result and isinstance(result["allAgents"], list)
         agents = result["allAgents"]
-        return {agent["agentId"]: agent for agent in agents}
+        return {agent["handle"]: agent for agent in agents}
 
     def sessions(self) -> List[str]:
         """Return a list of all session IDs."""
 
         query = gql(
             """
-            query getPlaygrounds {
-                allPlaygrounds {
+            query getSessions {
+                allSessions {
                     handle
                 }
             }
         """
         )
         result = self._gqlclient.execute(query)
-        assert "allPlaygrounds" in result and isinstance(result["allPlaygrounds"], list)
-        playgrounds = result["allPlaygrounds"]
-        return [playground["handle"] for playground in playgrounds]
+        assert "allSessions" in result and isinstance(result["allSessions"], list)
+        sessions = result["allSessions"]
+        return [session["handle"] for session in sessions]
 
     @property
-    def session_id(self) -> str:
+    def session_id(self) -> Optional[str]:
+        self._ensure_session()
         return self._session_id
 
-    def _create_session(self) -> str:
+    def _ensure_session(self):
+        """Create a new session if one does not already exist."""
+        if self._session_id:
+            return
+
         query = gql(
             """
-            mutation CreatePlayground {
-                createPlayground(playgroundData: {}) {
-                    playground {
+            mutation CreateSession {
+                createSession(sessionData: {}) {
+                    session {
                         handle
                     }
                 }
@@ -128,19 +127,20 @@ class FixieClient:
             """
         )
         result = self._gqlclient.execute(query)
-        if "createPlayground" not in result or result["createPlayground"] is None:
+        if "createSession" not in result or result["createSession"] is None:
             raise ValueError(f"Failed to create Session")
-        assert isinstance(result["createPlayground"], dict)
-        assert isinstance(result["createPlayground"]["playground"], dict)
-        assert isinstance(result["createPlayground"]["playground"]["handle"], str)
-        return result["createPlayground"]["playground"]["handle"]
+        assert isinstance(result["createSession"], dict)
+        assert isinstance(result["createSession"]["session"], dict)
+        assert isinstance(result["createSession"]["session"]["handle"], str)
+        self._session_id = result["createSession"]["session"]["handle"]
 
     def delete_session(self) -> None:
+        self._ensure_session()
         query = gql(
             """
-            mutation DeletePlayground($handle: String!) {
-                deletePlayground(handle: $handle) {
-                    playground {
+            mutation DeleteSession($handle: String!) {
+                deleteSession(handle: $handle) {
+                    session {
                         handle
                     }
                 }
@@ -150,10 +150,11 @@ class FixieClient:
         _ = self._gqlclient.execute(query, variable_values={"handle": self._session_id})
 
     def get_embeds(self) -> List[Dict[str, Any]]:
+        self._ensure_session()
         query = gql(
             """
             query getEmbeds($handle: String!) {
-                playgroundByHandle(handle: $handle) {
+                sessionByHandle(handle: $handle) {
                     embeds {
                         key
                         embed {
@@ -175,17 +176,18 @@ class FixieClient:
             query, variable_values={"handle": self._session_id}
         )
         assert (
-            "playgroundByHandle" in result
-            and isinstance(result["playgroundByHandle"], dict)
-            and isinstance(result["playgroundByHandle"]["embeds"], list)
+            "sessionByHandle" in result
+            and isinstance(result["sessionByHandle"], dict)
+            and isinstance(result["sessionByHandle"]["embeds"], list)
         )
-        return result["playgroundByHandle"]["embeds"]
+        return result["sessionByHandle"]["embeds"]
 
     def get_messages(self) -> List[Dict[str, Any]]:
+        self._ensure_session()
         query = gql(
             """
             query getMessages($handle: String!) {
-                playgroundByHandle(handle: $handle) {
+                sessionByHandle(handle: $handle) {
                     messages {
                         id
                         text
@@ -202,17 +204,17 @@ class FixieClient:
             query, variable_values={"handle": self._session_id}
         )
         assert (
-            "playgroundByHandle" in result
-            and isinstance(result["playgroundByHandle"], dict)
-            and isinstance(result["playgroundByHandle"]["messages"], list)
+            "sessionByHandle" in result
+            and isinstance(result["sessionByHandle"], dict)
+            and isinstance(result["sessionByHandle"]["messages"], list)
         )
-        return result["playgroundByHandle"]["messages"]
+        return result["sessionByHandle"]["messages"]
 
     def query(self, text: str) -> str:
         query = gql(
             """
             mutation Post($handle: String!, $text: String!) {
-                addPlaygroundMessage(messageData: {playground: $handle, text: $text}) {
+                addSessionMessage(messageData: {session: $handle, text: $text}) {
                     message {
                         text
                     }
