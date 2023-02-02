a6bf03b8825104147caeac5eed40cb3ffd5cd2ff
Hessam Bagherinezhad <hessam@fixie.ai>
2023-02-01 19:18:43 -0800

    Reformat

--- setup.py
+++ setup.py
@@ -1,38 +1,37 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-packages = ['llamalabs', 'llamalabs.agents', 'llamalabs.client']
+packages = ["llamalabs", "llamalabs.agents", "llamalabs.client"]
 
-package_data = {'': ['*']}
+package_data = {"": ["*"]}
 
 install_requires = [
-    'PyYAML>=6.0,<7.0',
-    'click>=8.1.3,<9.0.0',
-    'fastapi[all]>=0.89.1,<0.90.0',
-    'gql[all]>=3.4.0,<4.0.0',
-    'pillow>=9.0.1,<10.0.0',
-    'prompt-toolkit>=3.0.31,<4.0.0',
-    'pydantic>=1.4.0,<2.0.0',
-    'requests>=2.28.1,<3.0.0',
-    'rich>=12.6.0,<13.0.0',
+    "PyYAML>=6.0,<7.0",
+    "click>=8.1.3,<9.0.0",
+    "fastapi[all]>=0.89.1,<0.90.0",
+    "gql[all]>=3.4.0,<4.0.0",
+    "pillow>=9.0.1,<10.0.0",
+    "prompt-toolkit>=3.0.31,<4.0.0",
+    "pydantic>=1.4.0,<2.0.0",
+    "requests>=2.28.1,<3.0.0",
+    "rich>=12.6.0,<13.0.0",
 ]
 
 setup_kwargs = {
-    'name': 'llamalabs',
-    'version': '0.1.0',
-    'description': 'SDK for the Llama Labs platform. See: https://llamalabs.ai',
-    'long_description': 'None',
-    'author': 'LlamaLabs.ai Team',
-    'author_email': 'founders@fixie.ai',
-    'maintainer': 'None',
-    'maintainer_email': 'None',
-    'url': 'None',
-    'packages': packages,
-    'package_data': package_data,
-    'install_requires': install_requires,
-    'python_requires': '>=3.9,<4.0',
+    "name": "llamalabs",
+    "version": "0.1.0",
+    "description": "SDK for the Llama Labs platform. See: https://llamalabs.ai",
+    "long_description": "None",
+    "author": "LlamaLabs.ai Team",
+    "author_email": "founders@fixie.ai",
+    "maintainer": "None",
+    "maintainer_email": "None",
+    "url": "None",
+    "packages": packages,
+    "package_data": package_data,
+    "install_requires": install_requires,
+    "python_requires": ">=3.9,<4.0",
 }
 
 
 setup(**setup_kwargs)
-
