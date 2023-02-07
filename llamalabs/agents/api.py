77b8271f04c310c41c8103d7190d7a22adb28fee
Hessam Bagherinezhad <hessam@fixie.ai>
2023-02-06 22:59:02 -0800

    UserStorage binding in AgentSDK (#16)
    This adds a dict-like `UserStorage` object to agent SDK.

--- llamalabs/agents/api.py
+++ llamalabs/agents/api.py
@@ -1,9 +1,14 @@
+"""This module holds objects that represent the API interface by which Agents talk to
+Llama Labs ecosystem."""
+
 from __future__ import annotations
 
 import dataclasses
 from typing import Optional
 
 from pydantic import dataclasses as pydantic_dataclasses
+
+from llamalabs.agents import user_storage
 
 
 @pydantic_dataclasses.dataclass
@@ -36,16 +41,22 @@ class AgentQuery:
     # The contents of the query.
     message: Message
 
-    # The ID of the (human) user for whom this query was made. Agents should treat
-    # this as an opaque string and not make any assumptions about its format, other
-    # than that it uniquely identifies an end user.
-    originating_user_id: Optional[str] = None
+    # This is an access token associated with the user for whom this query was
+    # created. Agents wishing to make queries to other agents, or to other
+    # Fixie services, should carry this token in the query so that it
+    # can be tied back to the original user.
+    access_token: Optional[str] = None
 
-    # A session ID that can be used by agents to identify a given user's "session"
-    # while interacting with the Llama Labs system. Agents should treat this as an
-    # opaque string and not make any assumptions about its format, other than that
-    # it uniquely identifies a session for a given user.
-    session_id: Optional[str] = None
+    def user_storage(self, agent_id: str) -> user_storage.UserStorage:
+        """Returns a persistent, user-specific, dict-like memory storage.
+
+        Args:
+            agent_id: The agent who's asking for it.
+
+        TODO(hessam): Remove agent_id from the signature once agent_id is included in
+         access_token.
+        """
+        return user_storage.UserStorage(self, agent_id)
 
 
 @pydantic_dataclasses.dataclass
---
+++ llamalabs/agents/user_storage_test.py
@@ -0,0 +1,102 @@
+import doctest
+import re
+
+import pytest
+
+from llamalabs.agents import api
+from llamalabs.agents import user_storage
+
+FAKE_AGENT_ID = "fake-agent"
+FAKE_ACCESS_TOKEN = "fake-access-token"
+
+
+class MockUserStorageService:
+    """Mocks /api/userstorage behavior."""
+
+    def __init__(self):
+        self._data = {}
+
+    def _parse_request(self, request):
+        assert request.headers["Authorization"] == f"Bearer {FAKE_ACCESS_TOKEN}"
+        prefix = f"/api/userstorage/{FAKE_AGENT_ID}"
+        assert request.path.startswith(prefix)
+        key = request.path[len(prefix) + 1 :]
+        return key
+
+    def post(self, request, context):
+        key = self._parse_request(request)
+        assert key
+        self._data[key] = request.json()
+
+    def get(self, request, context):
+        key = self._parse_request(request)
+        if key:
+            return self._data[key]
+        else:
+            return [{"key": key, "value": value} for key, value in self._data.items()]
+
+    def head(self, request, context):
+        key = self._parse_request(request)
+        assert key
+        try:
+            self._data[key]
+        except KeyError:
+            context.status_code = 400
+            return {"msg": "key doesn't exist"}
+        else:
+            context.status_code = 200
+            return {"msg": "key exists"}
+
+    def delete(self, request, context):
+        key = self._parse_request(request)
+        assert key
+        del self._data[key]
+        return {"msg": "success"}
+
+
+@pytest.fixture
+def mock_user_storage_urls(requests_mock):
+    mock_storage_service = MockUserStorageService()
+    user_storage_path_re = re.compile(
+        "^" + re.escape(user_storage.USER_STORAGE_URL) + "/"
+    )
+    requests_mock.post(user_storage_path_re, json=mock_storage_service.post)
+    requests_mock.get(user_storage_path_re, json=mock_storage_service.get)
+    requests_mock.head(user_storage_path_re, json=mock_storage_service.head)
+    requests_mock.delete(user_storage_path_re, json=mock_storage_service.delete)
+
+
+VALUES_TO_TEST = [
+    1,
+    2.4,
+    True,
+    b"binary",
+    "simple_str",
+    {"key": "value"},
+    {"key1": {"key2": ["value1", True, b"binary2", None, 4.3]}},
+]
+
+
+@pytest.mark.parametrize("test_value", VALUES_TO_TEST)
+def test_user_storage(mock_user_storage_urls, test_value):
+    query = api.AgentQuery(
+        message=api.Message("sample query"), access_token=FAKE_ACCESS_TOKEN
+    )
+    storage = user_storage.UserStorage(query, agent_id=FAKE_AGENT_ID)
+
+    # Add the value and test its existence
+    storage["key"] = test_value
+    assert "key" in storage
+    assert len(storage) == 1
+    assert storage["key"] == test_value
+    assert list(storage) == ["key"]
+
+    del storage["key"]
+    assert "key" not in storage
+    assert len(storage) == 0
+    with pytest.raises(KeyError):
+        _ = storage["key"]
+
+
+def test_doctest(mock_user_storage_urls):
+    doctest.testmod(user_storage, raise_on_error=True)
