d3270f6a1779abc79316e17584a2fc535ea3f24a
Hessam Bagherinezhad <hessam@fixie.ai>
2023-02-14 13:43:46 -0800

    Decorator based SDK (#22)
    This makes our SDK a decorator based, instead of subclass based.
    
    See `simpleagent.py` for an example of the new SDK pattern.
    Or this new stack agent: https://replit.com/@fixie-ai/Stack#main.py
    
    This PR also adds:
    * Hit `fixie.ai/api/refresh/` endpoint upon startup.
    * Allow functions to be duck typed.

--- fixie/__init__.py
+++ fixie/__init__.py
@@ -1,3 +1,8 @@
+from fixie.agents.api import AgentQuery
+from fixie.agents.api import AgentResponse
+from fixie.agents.api import Embed
+from fixie.agents.api import Message
+from fixie.agents.code_shot import CodeShotAgent
 from fixie.client import FixieClient
 from fixie.client import get_agents
 from fixie.client import get_client
@@ -5,6 +10,11 @@ from fixie.client import get_embeds
 from fixie.client import query
 
 __all__ = [
+    "AgentQuery",
+    "AgentResponse",
+    "Embed",
+    "Message",
+    "CodeShotAgent",
     "FixieClient",
     "get_agents",
     "get_client",
--- fixie/agents/code_shot_test.py
+++ fixie/agents/code_shot_test.py
@@ -1,47 +1,60 @@
 import fastapi
+import pytest
 from fastapi import testclient
 
+import fixie
 from fixie import agents
+from fixie.agents import code_shot
+
+BASE_PROMPT = "I am a simple dummy agent."
+FEW_SHOTS = """
+Q: Sample query 1
+Ask Func[simple1]: Simple argument
+Func[simple1] says: Simple response
+A: Simple final response
+
+Q: Sample query 2
+Ask Func[simple2]: Simple argument
+Func[simple2] says: Simple response
+A: Simple final response
+"""
 
 
-class SimpleAgent(agents.CodeShotAgent):
-    agent_id = "simple_agent"
-    BASE_PROMPT = "I am a simple dummy agent."
-    FEW_SHOTS = [
-        """Q: Sample query 1
-           Ask Func[simple1]: Simple argument
-           Func[simple1] says: Simple response
-           A: Simple final response""",
-        """Q: Sample query 2
-           Ask Func[simple2]: Simple argument
-           Func[simple2] says: Simple response
-           A: Simple final response""",
-    ]
+@pytest.fixture
+def dummy_agent():
+    agent = agents.CodeShotAgent(BASE_PROMPT, FEW_SHOTS)
 
-    def simple1(self, query: agents.AgentQuery) -> str:
+    @agent.register_func
+    def simple1(query: agents.AgentQuery) -> str:
         return "Simple response 1"
 
-    def simple2(self, query: agents.AgentQuery) -> str:
+    @agent.register_func()
+    def simple2(query: agents.AgentQuery):
         return "Simple response 2"
 
+    @agent.register_func(func_name="custom")
+    def simple3(query):
+        return "Simple response custom"
 
-def test_simple_agent_handshake():
-    agent = SimpleAgent()
+    return agent
+
+
+def test_simple_agent_handshake(dummy_agent):
     fast_api = fastapi.FastAPI()
-    fast_api.include_router(agent.api_router())
+    fast_api.include_router(dummy_agent.api_router())
     client = testclient.TestClient(fast_api)
     response = client.get("/")
     assert response.status_code == 200
     json = response.json()
-    assert json["agent_id"] == agent.agent_id
-    assert json["base_prompt"] == agent.BASE_PROMPT
-    assert json["few_shots"] == agent.FEW_SHOTS
+    assert json == {
+        "base_prompt": dummy_agent.base_prompt,
+        "few_shots": dummy_agent.few_shots,
+    }
 
 
-def test_simple_agent_func_calls():
-    agent = SimpleAgent()
+def test_simple_agent_func_calls(dummy_agent):
     fast_api = fastapi.FastAPI()
-    fast_api.include_router(agent.api_router())
+    fast_api.include_router(dummy_agent.api_router())
     client = testclient.TestClient(fast_api, raise_server_exceptions=False)
 
     # Test Func[simple1]
@@ -56,16 +69,108 @@ def test_simple_agent_func_calls():
     json = response.json()
     assert json == {"message": {"text": "Simple response 2", "embeds": {}}}
 
+    # Test Func[custom]
+    response = client.post("/custom", json={"message": {"text": "Howdy"}})
+    assert response.status_code == 200
+    json = response.json()
+    assert json == {"message": {"text": "Simple response custom", "embeds": {}}}
+
     # Test non-existing Func[] returns 404: Not Found
-    response = client.post(
-        "/prefix/path/non_existing_func", json={"message": {"text": "Howdy"}}
-    )
+    response = client.post("/simple3", json={"message": {"text": "Howdy"}})
     assert response.status_code == 404
 
     # Test Func[simple1] with bad arguments returns 422: Unprocessable Entity
     response = client.post("/simple1", json={"message": {"ttt": "Howdy"}})
     assert response.status_code == 422
 
-    # Test Func[__init__] 403: Forbidden
+    # Test Func[__init__] 404: Not Found
     response = client.post("/__init__", json={"message": {"text": "Howdy"}})
-    assert response.status_code == 403
+    assert response.status_code == 404
+
+
+def test_split_few_shots():
+    few_shots_list = code_shot._split_few_shots(FEW_SHOTS)
+    assert few_shots_list == [
+        """
+Q: Sample query 1
+Ask Func[simple1]: Simple argument
+Func[simple1] says: Simple response
+A: Simple final response""",
+        """Q: Sample query 2
+Ask Func[simple2]: Simple argument
+Func[simple2] says: Simple response
+A: Simple final response""",
+    ]
+
+
+def good_duck_typed_func(x):
+    return "test"
+
+
+def bad_duck_typed_func1(x, y):
+    return "test"
+
+
+def bad_duck_typed_func2(*x):
+    return "test"
+
+
+def bad_duck_typed_func3(**x):
+    return "test"
+
+
+def good_typed_func1(x: fixie.AgentQuery) -> fixie.AgentResponse:
+    return fixie.AgentResponse(fixie.Message("test"))
+
+
+def good_typed_func2(x: fixie.AgentQuery) -> fixie.Message:
+    return fixie.Message("test")
+
+
+def good_typed_func3(x: fixie.AgentQuery) -> str:
+    return "test"
+
+
+def good_semi_typed_func4(x: fixie.AgentQuery):
+    ...
+
+
+def good_semi_typed_func5(x) -> str:
+    return "test"
+
+
+def bad_typed_func1(x: str) -> str:
+    return "test"
+
+
+def bad_typed_func2(x: int) -> str:
+    return "test"
+
+
+def test_registering_func(dummy_agent):
+    dummy_agent.register_func(good_typed_func1, func_name="name1")
+    dummy_agent.register_func(good_typed_func2, func_name="name2")
+    dummy_agent.register_func(good_typed_func3, func_name="name3")
+    assert dummy_agent._funcs["name1"] == good_typed_func1
+    assert dummy_agent._funcs["name2"] == good_typed_func2
+    assert dummy_agent._funcs["name3"] == good_typed_func3
+
+    dummy_agent.register_func(good_duck_typed_func)
+    assert dummy_agent._funcs["good_duck_typed_func"] == good_duck_typed_func
+
+    dummy_agent.register_func(good_semi_typed_func4)
+    assert dummy_agent._funcs["good_semi_typed_func4"] == good_semi_typed_func4
+
+    dummy_agent.register_func(good_semi_typed_func5)
+    assert dummy_agent._funcs["good_semi_typed_func5"] == good_semi_typed_func5
+
+    with pytest.raises(TypeError):
+        dummy_agent.register_func(bad_duck_typed_func1)
+    with pytest.raises(TypeError):
+        dummy_agent.register_func(bad_duck_typed_func2)
+    with pytest.raises(TypeError):
+        dummy_agent.register_func(bad_duck_typed_func3)
+    with pytest.raises(TypeError):
+        dummy_agent.register_func(bad_typed_func1)
+    with pytest.raises(TypeError):
+        dummy_agent.register_func(bad_typed_func2)
