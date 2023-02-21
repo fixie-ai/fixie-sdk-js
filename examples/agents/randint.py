190025cdd0560807024791ca0eaf0d9d17681396
Matt Welsh <matt@fixie.ai>
2023-02-20 18:00:10 -1000

    Formatting.

--- examples/agents/randint.py
+++ examples/agents/randint.py
@@ -3,9 +3,12 @@
 # This is an example Fixie Agent that generates random integers.
 
 import random
+
 import fixieai
 
-BASE_PROMPT = "I am a simple agent that generates a random number between two given values."
+BASE_PROMPT = (
+    "I am a simple agent that generates a random number between two given values."
+)
 FEW_SHOTS = """
 Q: Generate a random number between 0 and 19.
 Ask Func[genrand]: 0, 19
@@ -20,9 +23,11 @@ A: The random number is 8.
 
 agent = fixieai.CodeShotAgent(BASE_PROMPT, FEW_SHOTS)
 
+
 @agent.register_func()
 def genrand(query: fixieai.AgentQuery) -> str:
     low, high = query.message.text.replace(" ", "").split(",")
     return str(random.randint(int(low), int(high)))
+
 
 agent.serve("randint")
