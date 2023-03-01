1e01b360e885fa139f8855f3d11caeada15b6311
Justin Uberti <justin@uberti.name>
2023-02-28 17:30:16 -0800

    Return agent config in YAML format (#32)
    Looks a lot nicer when viewing it in the Replit window!

--- fixieai/agents/code_shot.py
+++ fixieai/agents/code_shot.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import dataclasses
 import functools
 import re
 import threading
@@ -8,6 +9,7 @@ from typing import Callable, Dict, List, Optional, Union
 import fastapi
 import requests
 import uvicorn
+import yaml
 from pydantic import dataclasses as pydantic_dataclasses
 
 from fixieai import constants
@@ -150,8 +152,11 @@ class CodeShotAgent:
         self._funcs[name] = func
         return func
 
-    def _handshake(self) -> AgentMetadata:
-        return AgentMetadata(self.base_prompt, self.few_shots)
+    def _handshake(self) -> fastapi.Response:
+        """Returns the agent's metadata in YAML format."""
+        metadata = AgentMetadata(self.base_prompt, self.few_shots)
+        yaml_content = yaml.dump(dataclasses.asdict(metadata))
+        return fastapi.Response(yaml_content, media_type="application/yaml")
 
     def _serve_func(self, func_name: str, query: AgentQuery) -> AgentResponse:
         try:
