ad7b7ad71dbd87ea64e1a697c38e551ed3d8bd3b
Peter Salas <peter@fixie.ai>
2023-03-08 17:09:11 -0800

    [cli] Implement fixie serve/deploy commands (#50)
    This implements `fixie agent serve` and `fixie agent deploy` commands
    (with a stub for pushing the actual deployment to resolve the
    chicken/egg problem of using the loader within the deployment itself).
    
    Some notes:
    - `AgentConfig.entry_point` is redefined for consistency with `uvicorn`
    such that `$MODULE:$ATTRIBUTE` points to an agent implementation
    (default is now `main:agent` instead of `main.py`).
    - Define a function that normalizes `Optional[str]` into a path to an
    `agent.yaml` file: `None` resolves to `./agent.yaml`, paths to
    directories resolve to `$path/agent.yaml`, otherwise strings are taken
    as-is.
    - Make `AgentConfig.deployment_url` explicitly optional -- when
    specified, `fixie agent deploy` updates the agent configuration and
    triggers a refresh. When _not_ specified, `fixie agent deploy` will
    deploy the agent as a Fixie-managed deployment.
    - Add support for updating `funcUrl` from `Agent`
    - Remove `fixie agent refresh` as it no longer has value distinct from
    `fixie agent deploy`

--- fixieai/client/client.py
+++ fixieai/client/client.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 import logging
-from typing import Any, Dict, List, Optional
+from typing import Any, BinaryIO, Dict, List, Optional
 
 import requests
 from gql import Client
@@ -119,8 +119,8 @@ class FixieClient:
     def create_agent(
         self,
         handle: str,
-        name: Optional[str] = None,
-        description: Optional[str] = None,
+        name: str,
+        description: str,
         query_url: Optional[str] = None,
         func_url: Optional[str] = None,
         more_info_url: Optional[str] = None,
@@ -192,4 +192,13 @@ class FixieClient:
         requests.post(
             f"{constants.FIXIE_REFRESH_URL}/{username}/{agent_handle}",
             headers=self._request_headers,
+        ).raise_for_status()
+
+    def deploy_agent(self, agent_handle: str, files: Dict[str, BinaryIO]):
+        """Deploys an agent implementation."""
+        username = self.get_current_username()
+        requests.post(
+            f"{constants.FIXIE_DEPLOYMENT_URL}/{username}/{agent_handle}",
+            headers=self._request_headers,
+            files=files,
         ).raise_for_status()
--- fixieai/constants.py
+++ fixieai/constants.py
@@ -22,6 +22,8 @@ FIXIE_REFRESH_URL = f"{FIXIE_API_URL}/api/refresh"
 # Fixie's OAuth redirect endpoint. Tokens sent to this endpoint will be redirected back
 # to your agent.
 FIXIE_OAUTH_REDIRECT_URL = f"{FIXIE_API_URL}/oauth"
+# Fixie's deployments service URL.
+FIXIE_DEPLOYMENT_URL = f"{FIXIE_API_URL}/api/deployments"
 # Fixie's JWKS URL.
 FIXIE_JWKS_URL = f"{FIXIE_API_URL}/.well-known/jwks.json"
 # Valid audiences for Fixie's query JWTs.
