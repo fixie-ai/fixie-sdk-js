d743d9631b359b0a47fc4398bf964e84522eda4e
Peter Salas <peter@fixie.ai>
2023-03-28 09:51:16 -0700

    Add response chunking and auth token verification to `GET /` (#100)
    This adds response chunking and auth token verification to `GET /`. For
    auth token verification, in addition to verifying that the response has
    a valid Fixie request token we also verify that the agent ID in the
    token is the expected ID.
    
    To avoid requiring that the agent ID be embedded in the _code_, the
    `fixie serve` and `fixie deploy` commands set the
    `FIXIE_ALLOWED_AGENT_ID` environment variable. The latter loads an
    `.env` file in the bootstrapping code. This environment variable is
    optional to avoid it being a breaking change -- we can make it required
    in the future. To avoid confusion between multiple uses of `agent_id` in
    `CodeShotAgent` this moves the refresh-on-startup logic entirely out of
    the agent implementation and into the agent loader where `fixie serve`
    can continue to use it.
    
    Finally, to allow agents to be deployed against non-prod Fixie
    environments, this uses the new `.env` mechanism to set `FIXIE_API_URL`
    to match the environment that the agent is being deployed into.

--- fixieai/agents/code_shot_test.py
+++ fixieai/agents/code_shot_test.py
@@ -1,4 +1,5 @@
 import dataclasses
+import os
 
 import fastapi
 import pytest
@@ -39,7 +40,8 @@ def mock_token_verifier(mocker):
 
 
 @pytest.fixture
-def dummy_agent():
+def dummy_agent(mocker):
+    mocker.patch.dict(os.environ, {"FIXIE_ALLOWED_AGENT_ID": "fake agent id"})
     agent = agents.CodeShotAgent(
         BASE_PROMPT,
         FEW_SHOTS,
@@ -72,11 +74,14 @@ def dummy_agent():
     return agent
 
 
-def test_simple_agent_handshake(dummy_agent):
+def test_simple_agent_handshake(dummy_agent, mocker):
     fast_api = fastapi.FastAPI()
     fast_api.include_router(dummy_agent.api_router())
     client = testclient.TestClient(fast_api)
-    response = client.get("/")
+
+    # Use a small chunk size to exercise chunking logic
+    mocker.patch.object(code_shot, "_RESPONSE_CHUNK_SIZE", 10)
+    response = client.get("/", headers={"Authorization": "Bearer fixie-test-token"})
     assert response.status_code == 200
     assert response.headers["content-type"] == "application/yaml"
     yaml_content = yaml.load(response.content, Loader=yaml.Loader)
@@ -107,7 +112,7 @@ def test_simple_agent_func_calls(dummy_agent, mock_token_verifier):
     json = response.json()
     assert json == {"message": {"text": "Simple response 1", "embeds": {}}}
     mock_token_verifier.assert_called_once_with(
-        "fixie-test-token", dummy_agent._jwks_client
+        "fixie-test-token", dummy_agent._jwks_client, dummy_agent._allowed_agent_id
     )
 
     # Test Func[simple2]
