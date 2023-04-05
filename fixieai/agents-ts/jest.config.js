f1bc52b3ed468c5d17e58c65629e544c0d3465d0
Nick Heiner <nick.heiner+github@gmail.com>
2023-04-05 14:38:44 -0700

    Try to use got + esm but it is a hot mess

--- fixieai/agents-ts/jest.config.js
+++ fixieai/agents-ts/jest.config.js
@@ -1,5 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+  extensionsToTreatAsEsm: ['.ts'],
+  preset: 'ts-jest/presets/default-esm', // or other ESM presets
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.m?[tj]sx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: 'tsconfig.json',
+      },
+    ],
+  },
+}
--- fixieai/agents-ts/src/__tests__/serve.ts
+++ fixieai/agents-ts/src/__tests__/serve.ts
@@ -1,5 +1,36 @@
-import serve from '../serve';
+import fs from 'fs';
+import got from 'got';
+import yaml from 'js-yaml';
+import path from 'path';
+import serve, { AgentConfig } from '../serve';
+import { fileURLToPath } from 'url';
 
-it('test', () => {
-  expect(1+1).toBe(2)
-})
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const agentConfigPath = path.join(__dirname, 'test_agent.yaml');
+const agentConfigContents = fs.readFileSync(agentConfigPath, 'utf8');
+const agentConfig = yaml.load(agentConfigContents) as AgentConfig;
+
+describe('error handling', () => {
+  it('throws an error if the entry point does not exist', () => {
+    expect(() => serve('agent.yaml', { 
+      ...agentConfig,
+      entry_point: 'does-not-exist.ts' 
+    }, 3000, true)).toThrow();
+  });
+});
+
+it('Agent metadata', async () => {
+  const port = 3000;
+  const silentStartup = true;
+
+  await serve(agentConfigPath, agentConfig, port, silentStartup);
+
+  const response = await got(`http://localhost:${port}`);
+
+  expect(response.statusCode).toBe(200);
+  expect(response.body).toEqual(JSON.stringify({
+    base_prompt: "Hello! I'm a test agent. How can I help you today?",
+    few_shots: ['What can you do?', 'How do I get started?'],
+  }));
+});
