312f1429dae8983c973957b08a9832b2d8355e37
Nick Heiner <nick.heiner+github@gmail.com>
2023-04-10 17:19:08 -0700

    Initial user storage implementation

--- fixieai/agents-ts/src/user-storage.ts
+++ fixieai/agents-ts/src/user-storage.ts
@@ -1,3 +1,77 @@
+import got, { Got, Response } from 'got';
+import { Jsonifiable, JsonValue } from 'type-fest';
+
+class GotError extends Error {
+  constructor(message: string, public readonly response: Response) {
+    super(message);
+  }
+}
+
+/**
+ * https://docs.fixie.ai/agents/#user-storage
+ *
+ * A client for Fixie's user storage API.
+ */
 export class UserStorage {
-  constructor(private readonly apiUrl: string) {}
+  private readonly gotClient: Got;
+
+  constructor(apiUrl: string, agentId: string) {
+    this.gotClient = got.extend({
+      prefixUrl: `${apiUrl}/${agentId}/`,
+      responseType: 'json',
+      headers: {
+        Authorization: 'Bearer TODO',
+      },
+    });
+  }
+
+  /**
+   * Get the value for a key. If the key does not exist, an error will be thrown.
+   */
+  get(key: string): Promise<JsonValue> {
+    return this.gotClient.get(key).json();
+  }
+
+  /**
+   * Get all saved keys.
+   */
+  getKeys(): Promise<string[]> {
+    return this.gotClient.get('').json();
+  }
+
+  /**
+   * Set a value for a key.
+   */
+  async set(key: string, value: Jsonifiable): Promise<void> {
+    await this.gotClient.post(key, {
+      // Got's types are incorrectly restrictive, so we'll cast.
+      json: value as Record<string, any>,
+    });
+  }
+
+  /**
+   * Check if a key exists in storage.
+   */
+  async has(key: string): Promise<boolean> {
+    const response = await this.gotClient.head(key, {
+      throwHttpErrors: false,
+    });
+    if (response.statusCode === 404) {
+      return false;
+    }
+    if (response.statusCode === 200) {
+      return true;
+    }
+    throw new GotError(
+      `Fixie API: Unexpected status code ${response.statusCode} when checking for key ${key}. This is probably not your fault.`,
+      response,
+    );
+  }
+
+  /**
+   * Delete a key. If the key does not already exist, an error will be thrown.
+   */
+  async delete(key: string): Promise<void> {
+    await this.gotClient.delete(key);
+  }
 }
