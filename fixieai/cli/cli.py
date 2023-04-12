9192ee6d8d9c858abd365b7c04ab303a8f5f0ef7
Matt Welsh <matt@fixie.ai>
2023-04-12 08:17:12 -0700

    Add self-check for version update. (#150)
    This PR adds code to show a warning to the user if there is a newer
    version of the `fixieai` package on PyPi when they run the CLI.

--- fixieai/cli/cli.py
+++ fixieai/cli/cli.py
@@ -3,6 +3,9 @@
 from typing import Optional
 
 import click
+import pkg_resources
+import requests
+from packaging import version
 
 import fixieai
 from fixieai import constants
@@ -54,5 +57,40 @@ fixie.add_command(session_commands.new_session, "console")
 fixie.add_command(session_commands.query, "query")
 
 
+def get_installed_version(package_name):
+    try:
+        # Get the distribution object for the specified package
+        distribution = pkg_resources.get_distribution(package_name)
+        # Get the version of the installed package
+        installed_version = distribution.version
+        return installed_version
+    except pkg_resources.DistributionNotFound:
+        # Handle the case where the package is not found
+        return None
+
+
+def check_for_update(package_name):
+    installed_version = get_installed_version(package_name)
+    try:
+        response = requests.get(f"https://pypi.org/pypi/{package_name}/json", timeout=3)
+        response.raise_for_status()
+    except Exception as e:
+        click.secho(f"An error occurred while checking for updates: {e}", fg="yellow")
+        return
+
+    package_info = response.json()
+    latest_version = package_info["info"]["version"]
+    if version.parse(installed_version) < version.parse(latest_version):
+        click.secho(
+            f"🦊 A newer version of {package_name} is available: {latest_version}",
+            fg="green",
+        )
+
+        click.secho(f"You are currently using version {installed_version}", fg="yellow")
+        click.secho(f"You can upgrade using:", fg="yellow")
+        click.secho(f"   pip install --upgrade {package_name}\n\n", fg="blue")
+
+
 if __name__ == "__main__":
+    check_for_update("fixieai")
     fixie()
