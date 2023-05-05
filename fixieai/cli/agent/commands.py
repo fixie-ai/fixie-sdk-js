cd225d0f9baea97f7135b7c7c6d3100c338dc242
Peter Salas <peter@fixie.ai>
2023-05-05 09:39:27 -0700

    Fix incorrect type for uvicorn port argument (#170)
    The CLI incorrectly passed the port to uvicorn as a string, rather than
    an integer, causing log spew on startup.

--- fixieai/cli/agent/commands.py
+++ fixieai/cli/agent/commands.py
@@ -473,7 +473,8 @@ if __name__ == "__main__":
     import uvicorn
 
     from fixieai.cli.agent import loader
-    uvicorn.run(loader.uvicorn_app_factory(), host="0.0.0.0", port=os.getenv("PORT", "8080"))
+    port = int(os.getenv("PORT", 8080))
+    uvicorn.run(loader.uvicorn_app_factory(), host="0.0.0.0", port=port)
 """
 
 
