aba12e4aff0e2059834f6fdad9d1a85261fd9950
juberti <justin@fixie.ai>
2023-12-16 22:35:29 -0800

    allow keywords to be specified

--- packages/fixie/src/voice.ts
+++ packages/fixie/src/voice.ts
@@ -28,6 +28,7 @@ export enum VoiceSessionState {
 export interface VoiceSessionInit {
   asrProvider?: string;
   asrLanguage?: string;
+  asrKeywords?: string[];
   model?: string;
   ttsProvider?: string;
   ttsModel?: string;
@@ -224,6 +225,7 @@ export class VoiceSession {
         asr: {
           provider: this.params?.asrProvider,
           language: this.params?.asrLanguage,
+          keywords: this.params?.asrKeywords,
         },
         tts: {
           provider: this.params?.ttsProvider,
